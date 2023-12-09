eb80526d69e75aff5042499745ac9b460eb7f520
DESKTOP-1SDFEC4\keisuke(ByDELL) <keisuke.wakasa.s11t286@gmail.com>
2023-12-09 17:43:40 +0900

    修正

--- MineSweeper/main.py
+++ MineSweeper/main.py
@@ -7,6 +7,11 @@ class BoardGrid:
         self.square_num = 20
         self.screen_width = 1000
         self.screen_height = 1000
+        
+        #爆弾数
+        self.bomb_count = 15
+        #旗の数は爆弾数 + 5とする
+        self.flag_count = self.bomb_count + 5
 
         self.screen = pygame.display.set_mode((self.screen_width, self.screen_height))
         #１マスの大きさ(50)
@@ -26,13 +31,13 @@ class BoardGrid:
         self.font = pygame.font.SysFont(None, 100)
         self.font2 = pygame.font.SysFont(None, 40)
 
-        self.win_black = self.font.render("黒が勝ったよ！", False, self.BLACK, self.RED)
-        self.win_white = self.font.render("白が勝ったよ！", False, self.WHITE, self.RED)
+        self.success = self.font.render("SUCCESS!!", False, self.BLACK, self.RED)
+        self.lose = self.font.render("LOSE....", False, self.WHITE, self.RED)
         self.drow = self.font.render("引き分け！", False, self.BLUE, self.RED)
         self.restart = self.font.render("リスタート", False, self.BLUE, self.RED)
 
         # 盤面作成
-        self.board =self.init_board(7)
+        self.board =self.init_board(self.bomb_count)
     
     def draw_grid(self):
         for i in range(self.square_num):
@@ -50,14 +55,41 @@ class BoardGrid:
                               (i * self.suqare_size, self.screen_height),
                               3
                                 )
+    # 盤面内の描画を行う
     def draw_board(self):
         for row_index, row in enumerate(self.board):
             for col_index,col in enumerate(row):
                 # 周りの地雷数が１より大きいとき
-                if col >=  0:
+                if col >=  0 and col <=8:
                     pygame.draw.rect(self.screen, self.SNOW, (col_index * self.suqare_size, row_index* self.suqare_size, 50, 50))
                     text =  self.font2.render(str(col), True, self.BLACK) 
                     self.screen.blit(text, [col_index * self.suqare_size + 15,  row_index* self.suqare_size+10])
+                #爆弾を描画
+                elif col == 11:
+                    img1 = pygame.image.load("bomb.png")
+                    img1 = pygame.transform.scale(img1, (50, 50)) #200 * 130に画像を縮小
+                    self.screen.blit(img1, [col_index * self.suqare_size ,  row_index* self.suqare_size])
+                # 旗を描画
+                elif col == 10:
+                    img1 = pygame.image.load("flag.png")
+                    img1 = pygame.transform.scale(img1, (50, 50)) #200 * 130に画像を縮小
+                    self.screen.blit(img1, [col_index * self.suqare_size ,  row_index* self.suqare_size])
+    
+    # 爆弾と旗の数を描画する
+    def draw_bomb_flag_count(self):
+        bomb_msg =  "Bomb: " + str(self.bomb_count)
+        bomb_txt =  self.font2.render(bomb_msg, True, self.RED) 
+        self.screen.blit(bomb_txt, [2 , 0])
+
+        usage_flag_count = 0
+        # 現在使用された旗をカウント
+        for i in self.board:
+            usage_flag_count += i.count(10)
+        #残りの旗を表示
+        flag_msg =  "Flag: " + str(self.flag_count - usage_flag_count)
+        flag_txt =  self.font2.render(flag_msg, True, self.GREEN) 
+        self.screen.blit(flag_txt, [150 , 0])
+
     
     def init_board(self, bombCount):
         board = []
@@ -126,6 +158,9 @@ def main():
     # 定数
     FPS = 60
     clock = pygame.time.Clock()
+    LEFT = 1
+    RIGHT = 3
+
 
     game_over = False
 
@@ -137,29 +172,78 @@ def main():
         boardGrid.screen.fill(boardGrid.GREY)
         boardGrid.draw_grid()
         boardGrid.draw_board()
+        boardGrid.draw_bomb_flag_count()
 
+        #ゲームオーバー時の処理
+        if game_over:
+
+            
+            #爆弾にヒットしていた場合
+
+
+
+            #失敗のメッセージ表示
+            boardGrid.screen.blit(boardGrid.lose,(230,200))
+
+
+
+        # イベント処理
         for evet in pygame.event.get():
             if evet.type == pygame.QUIT:
                 run = False
             
-            # マウスがクリックされたとき
+            # クリックされたとき
             if evet.type == pygame.MOUSEBUTTONDOWN:
                 mx, my =pygame.mouse.get_pos()
                 x = mx // boardGrid.suqare_size
                 y = my // boardGrid.suqare_size
                 print(f'{x}、{y}')
 
-                # 地雷かどうか判別
-                if play.check_bomb(boardGrid, x, y):
-                    # 地雷なら終了
-                    game_over = True
-                    print("OUT!!!!!")
+                if game_over:
+                    #ゲームオーバーの場合、再スタート
+                    game_over = False
+                    # ボード再作成（初期化）
+                    boardGrid = BoardGrid()
                 else:
-                    # 地雷でなければ周りの地雷数取得して、設定
-                    count = play.search_inspector(boardGrid, x, y)
-                    print(count)
-                    boardGrid.board[y][x] = count
+                    if boardGrid.board[y][x] >= 0:
+                        # 既にクリックされている個所の場合、スキップ
+                        continue
 
+
+                    # 左クリックされたとき
+                    if evet.button == LEFT:
+                        # 地雷かどうか判別
+                        if play.check_bomb(boardGrid, x, y):
+                            # 地雷なら終了
+                            game_over = True
+                            print("OUT!!!!!")
+
+                            #爆弾設定(爆弾:11)
+                            boardGrid.board[y][x] = 11
+
+                           
+
+                            
+                        else:
+                            # 地雷でなければ周りの地雷数取得して、設定
+                            count = play.search_inspector(boardGrid, x, y)
+                            print(count)
+                            # 0～8のどれか
+                            boardGrid.board[y][x] = count
+                
+                    # 右クリックの場合は旗を立てる(旗:10)
+                    elif evet.button == RIGHT:
+
+                        # 残りの旗数をチェック
+                        
+                        usage_flag_count = 0
+                        # 現在使用された旗をカウント
+                        for i in boardGrid.board:
+                            usage_flag_count += i.count(10)
+                        if boardGrid.flag_count > usage_flag_count:
+                            # まだ旗が残っていたら旗を設定
+                            boardGrid.board[y][x] = 10
+    
 
 
 
