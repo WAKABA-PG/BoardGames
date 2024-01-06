338d3e5f56bc738dd759ad54a6de3f60aa1cda09
DESKTOP-1SDFEC4\keisuke(ByDELL) <keisuke.wakasa.s11t286@gmail.com>
2024-01-06 16:19:38 +0900

    はるたか君の修正を反映

--- MineSweeper/main.py
+++ MineSweeper/main.py
@@ -1,17 +1,14 @@
 import pygame
 import random
-
 class BoardGrid:
     def __init__(self):
         # 変数を定義
         self.square_num = 20
         self.screen_width = 1000
         self.screen_height = 1000
-
         self.screen = pygame.display.set_mode((self.screen_width,self.screen_height))
         # 1マスの大きさ
         self.square_size = self.screen_width // self.square_num
-
         # 色
         self.BLACK = (0,0,0)
         self.WHITE = (255,255,255)
@@ -21,12 +18,10 @@ class BoardGrid:
         self.YELLOW = (255,255,0)
         self.DODGER_BLUE = (30,144,255)
         self.DEEP_SKY_BLUE = (0,191,255)
-
         self.font = pygame.font.SysFont(None, 40)
         self.game_over = self.font.render("GAME OVER", False, self.RED, self.BLACK)
         self.clear = self.font.render("CLEAR!", False, self.YELLOW, self.BLUE)
         self.restart = self.font.render("RESTART", False, self.BLUE, self.YELLOW)
-
         self.board = [[-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2],
                       [-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2],
                       [-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2],
@@ -49,14 +44,12 @@ class BoardGrid:
                       [-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2]]
         # ランダム設定
         self.random = self.square_num * self.square_num // 8
-        
         while self.random > 0:
             j = random.randint(0, self.square_num - 1)
             l = random.randint(0, self.square_num - 1)
             if self.board[j][l] == -2:
                 self.board[j][l] = -1
                 self.random -= 1
-
         print(j)
         print(self.board)
     def draw_grid(self):
@@ -74,12 +67,11 @@ class BoardGrid:
                              (i * self.square_size, 0),
                              (i * self.square_size, self.screen_width),
                              2
-                             )   
+                             )
     def draw_board(self):
         for row_index, row in enumerate(self.board):
             for col_index, col in enumerate(row):
                 if 0 <= col <= 8:
-
                 # number
                     pygame.draw.rect(
                     self.screen,
@@ -88,10 +80,23 @@ class BoardGrid:
                     row_index * self.square_size,
                     50,
                     50)
-                    ) 
+                    )
                     text = self.font.render(str(col), True, self.YELLOW)
                     self.screen.blit(text, [col_index * self.square_size + 15, row_index * self.square_size + 10])
-        
+                if col == -3:
+                    # TODO：エラーあり
+                    for col in self.square_num * self.square_num // 8:
+                        pygame.draw.rect(
+                        self.screen,
+                        self.RED,
+                        (col_index * self.square_size,
+                        row_index * self.square_size,
+                        50,
+                        50)
+                        )
+                        img1 = pygame.image.load("bomb.png")
+                        img1_2 = pygame.transform.scale(img1,(50,50))
+                        self.screen.blit(img1_2,[col_index * self.square_size,row_index * self.square_size])
 class Play:
     def __init__(self):
         self.vec_table = [
@@ -104,7 +109,6 @@ class Play:
             (0,1),
             (1,1)
         ]
-    
     def flip_pieces(self, col, row, boardGrid):
         print(col)
         print(row)
@@ -112,34 +116,31 @@ class Play:
         for vx, vy in self.vec_table:
             x = vx + col
             y = vy + row
-            if boardGrid.board[y][x] == -1:
-                bomb_list += 1    
+            if (0<= x < boardGrid.square_num
+                and 0 <= y < boardGrid.square_num
+                and boardGrid.board[y][x] == -1):
+                bomb_list += 1
         print (bomb_list)
-
-        if boardGrid.board[y][x] == -2:
-            boardGrid.board[y][x] = bomb_list
-
+        if boardGrid.board[row][col] == -2:
+            boardGrid.board[row][col] = bomb_list
+        elif boardGrid.board[row][col] == -1:
+            boardGrid.board[row][col] = -3
 def main():
     pygame.init()
     pygame.display.set_caption("マインスイーパー")
-
     # 定数
     FPS = 60
     clock = pygame.time.Clock()
     game_over = False
     run = True
-
     boardGrid = BoardGrid()
     play = Play()
-
     while run:
         boardGrid.screen.fill(boardGrid.DEEP_SKY_BLUE)
         boardGrid.draw_grid()
         boardGrid.draw_board()
-
         if game_over:
             boardGrid.screen.blit(boardGrid,game_over (200,200))
-
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
@@ -153,8 +154,6 @@ def main():
                     x = mx // boardGrid.square_size
                     y = my // boardGrid.square_size
                     play.flip_pieces(x, y, boardGrid)
-
         pygame.display.update()
         clock.tick(FPS)
-
-main()        
+main()
